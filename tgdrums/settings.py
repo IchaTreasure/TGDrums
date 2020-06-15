d0ad63fda3f79bd07aa623ad0bc1741fa9c07b67
Ubuntu <ubuntu@ip-172-31-4-78.eu-west-2.compute.internal>
2020-06-15 15:33:30 +0000

    hiding secret key

--- tgdrums/settings.py
+++ tgdrums/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_74ul7u$ccig2zifh*i$so(@7vmcm+13&)*_y38!c)v(c61a9*'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
