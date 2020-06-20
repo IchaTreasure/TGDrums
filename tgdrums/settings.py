b96a2f9b6f6819d51bcc151132bd81d15702c71e
Ubuntu <ubuntu@ip-172-31-4-78.eu-west-2.compute.internal>
2020-06-20 17:45:46 +0000

    fixed requirements.txt file for deployment

--- tgdrums/settings.py
+++ tgdrums/settings.py
@@ -12,8 +12,9 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import dj_database_url
-import env 
 
+if os.path.exists("env.py"):
+    import env 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -79,7 +80,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'tgdrums.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
