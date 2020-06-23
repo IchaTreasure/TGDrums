131b63a4b9f3c31a4472be6af7735bdd523459e7
Ubuntu <ubuntu@ip-172-31-4-78.eu-west-2.compute.internal>
2020-06-23 18:34:04 +0000

    edited checkout, product detail and cart html pages

--- tgdrums/settings.py
+++ tgdrums/settings.py
@@ -84,11 +84,11 @@ WSGI_APPLICATION = 'tgdrums.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-#if "DATABASE_URL" in os.environ:
-#    DATABASES = {'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))}
-#else:
-#    print("Database URL not found ")
-DATABASES = {
+if "DATABASE_URL" in os.environ:
+    DATABASES = {'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))}
+else:
+    print("Database URL not found ")
+    DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
