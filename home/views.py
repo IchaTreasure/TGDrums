5aba80f59845a6519366e334836bc62064c152ec
Ubuntu <ubuntu@ip-172-31-4-78.eu-west-2.compute.internal>
2020-06-28 23:47:25 +0000

    validated python files in home app using pep8

--- home/views.py
+++ home/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def index(request):
     """A view that displayes an index page"""
-    return render (request, "index.html")
-    
+    return render(request, "index.html")
+
+
 def homepage(request):
     """A view that displayes a home page"""
-    return render (request, "home.html")
-    
+    return render(request, "home.html")
+
+
 def aboutUs(request):
     """A view that displayes an about us page"""
-    return render (request, "about.html")
+    return render(request, "about.html")
