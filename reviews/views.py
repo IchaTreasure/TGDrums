7902ecc787d86ae5894a9f7b2e309124410a9f79
Ubuntu <ubuntu@ip-172-31-4-78.eu-west-2.compute.internal>
2020-06-29 00:20:24 +0000

    validated python files in reviews app using pep8

--- reviews/views.py
+++ reviews/views.py
@@ -4,19 +4,20 @@ from django.utils import timezone
 from .models import Post
 from .forms import ReviewPostForm
 
+
 # Create your views here.
 def get_posts(request):
     """
     Create a view that returns reviews that have already been posted
     """
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
-    
     return render (request, "reviewposts.html", {'posts': posts})
-    
+
+
 def post_detail(request, pk):
     """
     Create a view to return a single review object based on the Post ID(pk)
-    and render it to reviewdetails.html template or return a 404 error if no 
+    and render it to reviewdetails.html template or return a 404 error if no
     post is found
     """
     post = get_object_or_404(Post, pk=pk)
@@ -24,7 +25,8 @@ def post_detail(request, pk):
     post.save()
     return render(request, "reviewdetails.html", {'post': post})
 
-@login_required()  
+
+@login_required()
 def create_or_edit_post(request, pk=None):
     post = get_object_or_404(Post, pk=pk) if pk else None
     if request.method == "POST":
@@ -35,5 +37,3 @@ def create_or_edit_post(request, pk=None):
     else:
         form = ReviewPostForm(instance=post)
     return render(request, 'reviewforms.html', {'form': form})
-    
-    
