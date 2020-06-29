740c9cf4dc6ce52e4a521c46b28841fe6a5cb339
Ubuntu <ubuntu@ip-172-31-4-78.eu-west-2.compute.internal>
2020-06-29 00:40:35 +0000

    validated python files in search app and settings.py using pep8

--- tgdrums/settings.py
+++ tgdrums/settings.py
@@ -14,7 +14,7 @@ import os
 import dj_database_url
 
 if os.path.exists("env.py"):
-    import env 
+    import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -152,7 +152,7 @@ STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'), 
+    os.path.join(BASE_DIR, 'static'),
 )
 
 MEDIAFILES_LOCATION = 'media'
--- tgdrums/urls.py
+++ tgdrums/urls.py
@@ -1,17 +1,5 @@
-"""tgdrums URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+tgdrums URL Configuration
 """
 from django.conf.urls import url, include
 from django.contrib import admin
@@ -42,5 +30,3 @@ urlpatterns = [
     url(r'^posts/', include(urls_posts)),
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT})
 ]
-
-"""url(r'^$', all_products, name='index'),"""
